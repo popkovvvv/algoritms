b8b5c9a58ac020e2add543acb001d9fbd8cfae6d
popkovvvv <popkovvvv@mail.ru>
2019-11-13 10:12:24 +0300

    Add QuickSort

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,4 +1,8 @@
 import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Application {
     public static void main(String[] args) {
@@ -13,6 +17,18 @@ public class Application {
 
 
         System.out.println(BinarySort.binarySort(arr,13));
+
+        int[] x = { 8, 0, 4, 7, 3, 7, 10, 12, -3 };
+        System.out.println("Было");
+        System.out.println(Arrays.toString(x));
+
+        int low = 0;
+        int high = x.length - 1;
+
+        QuickSort.quickSort(x, low, high);
+        System.out.println("Стало");
+        System.out.println(Arrays.toString(x));
     }
+
 
 }
